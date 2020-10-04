e9692456819dc41c1ec2ca4dd5bfae85847f718c
Rochelle Ruiz <rcelevante@gmail.com>
2020-10-04 16:19:09 -0700

    Updates

---
+++ precache-manifest.6c42cbed1d70bec31cea6a3d5f4ca142.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6c23f10abd4f8e36efe2f9a66c8258",
+    "url": "/rcelevante/PORTFOLIO_reactindex.html"
+  },
+  {
+    "revision": "d217dacbdaf083372fa3",
+    "url": "/rcelevante/PORTFOLIO_reactstatic/css/main.7c2796e1.chunk.css"
+  },
+  {
+    "revision": "fd620c9220a06e5f276b",
+    "url": "/rcelevante/PORTFOLIO_reactstatic/js/2.79c54a71.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rcelevante/PORTFOLIO_reactstatic/js/2.79c54a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d217dacbdaf083372fa3",
+    "url": "/rcelevante/PORTFOLIO_reactstatic/js/main.93c84726.chunk.js"
+  },
+  {
+    "revision": "4c1ffe1708bd02c5b4b5",
+    "url": "/rcelevante/PORTFOLIO_reactstatic/js/runtime-main.9f38b56e.js"
+  },
+  {
+    "revision": "a082dfb8c57d0400e21316558fd20da4",
+    "url": "/rcelevante/PORTFOLIO_reactstatic/media/Resume.a082dfb8.pdf"
+  },
+  {
+    "revision": "c271f1a731e9b430b3c9efa646337de0",
+    "url": "/rcelevante/PORTFOLIO_reactstatic/media/background.c271f1a7.jpeg"
+  }
+]);
